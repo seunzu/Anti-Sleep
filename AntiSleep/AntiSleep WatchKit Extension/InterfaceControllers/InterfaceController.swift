a67f738391ae642e88566ff5a7384f50f125670e
zzoo0123 <0123suh@gmail.com>
2023-01-27 03:53:21 +0900

    heartRate 출력확인

--- AntiSleep/AntiSleep WatchKit Extension/InterfaceControllers/InterfaceController.swift
+++ AntiSleep/AntiSleep WatchKit Extension/InterfaceControllers/InterfaceController.swift
@@ -15,24 +15,14 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet weak var driveBtn: WKInterfaceButton!
     @IBOutlet weak var heartRateCountLabel: WKInterfaceLabel!
     @IBOutlet weak var sleepCountLabel: WKInterfaceLabel!
+    @IBOutlet weak var driveTimer: WKInterfaceTimer!
     
     var sleepCount : Int = 0
     let healthStore = HKHealthStore()
-    let heartRateQuantity = HKUnit(from: "count/min")
-    var value : Int = 0
-
-    // 심박수, 수면시간 권한 요청
-    let typeToRead = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!,
-                    HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!])
-    let typeToShare = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!,
-                    HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!])
-    var sleepData:[HKCategorySample] = [] // 수면 데이터 저장 배열
-
-    var locationManager = CLLocationManager()
 
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
-        configure()
+        authorizeHealthKit()
     }
 
     override func willActivate() {
@@ -44,41 +34,35 @@ class InterfaceController: WKInterfaceController {
     }
 
     @IBAction func dBtnTapped() {
-        //self.pushController(withName: "drive", context: nil)
         driveBtn.setTitle("Driving")
     }
 
-    func setLabel() {
-        self.sleepCountLabel.setText("졸음 운전 횟수 : \(sleepCount)")
-        self.heartRateCountLabel.setText("\(value)")
-    }
-
-    func configure() {
-        if !HKHealthStore.isHealthDataAvailable() {
-            //
-        } else {
-            requestAuthorization()
-        }
-    }
-
     // healthKit 권한 요청
-    func requestAuthorization() {
-        self.healthStore.requestAuthorization(toShare: typeToShare, read: typeToRead) { (success, error) in
+    func authorizeHealthKit() {
+        // 심박수, 수면시간 권한 요청
+        let typeToRead = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!,
+                        HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!])
+        let typeToShare = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!,
+                        HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!])
+
+        healthStore.requestAuthorization(toShare: typeToShare, read: typeToRead) { (success, error) in
             if error != nil {
                 print(error.debugDescription)
             } else {
                 if success {
                     print("권한이 허락되었습니다.")
+                    self.getHeartRateData()
                 } else {
                     print("권한이 아직 없습니다.")
                 }
             }
         }
     }
-
+    
     // 심장 박동수 가져오기
     // https://ios-dev-tech.tistory.com/12
-    func getHeartRateData(completion: @escaping ([HKSample]) -> Void) {
+    // https://www.youtube.com/watch?v=uzJXV_9IBoc
+    func getHeartRateData() {
         // HKObjectType: HealthKitStore 대한 특정 유형의 데이터 식별 클래스
         // HKObjectType.quantityType: identifier에 대한 수량 유형 반환
         guard let sampleType = HKObjectType.quantityType(forIdentifier: .heartRate) else {
@@ -89,22 +73,23 @@ class InterfaceController: WKInterfaceController {
         // HKQuery: HealthKit 안 모든 query class 위한 추상 클래스
         // HKQuery.predicateForSamples: 특정 시간 동안의 특정 데이터 반환
         let predicate = HKQuery.predicateForSamples(withStart: startDate, end: Date(), options: .strictEndDate)
-        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: true)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
         let query = HKSampleQuery(sampleType: sampleType, predicate: predicate, limit: Int(HKObjectQueryNoLimit), sortDescriptors: [sortDescriptor]) {
             (sample, result, error) in
             guard error == nil else {
-                print("error")
                 return
             }
-            guard let resultData = result else {
-                print("load result fail")
-                return
-            }
-            DispatchQueue.main.async {
-                completion(resultData)
-            }
+            let data = result![0] as! HKQuantitySample
+            let unit = HKUnit(from: "count/min")
+            let latesHr = data.quantity.doubleValue(for: unit)
+            print("HeartRate : \(latesHr) BPM")
+            
+            let dateFormator = DateFormatter()
+            dateFormator.dateFormat = "dd/MM/yyyy hh:mm s"
+            let StartDate = dateFormator.string(from: data.startDate)
+            let EndDate = dateFormator.string(from: data.endDate)
+            print("StartDate \(StartDate) : EndDate \(EndDate)")
         }
         healthStore.execute(query)
     }
-
 }
--- AntiSleep/AntiSleep/ViewController.swift
+++ AntiSleep/AntiSleep/ViewController.swift
@@ -5,105 +5,83 @@
 //  Created by suhseungju on 2023/01/21.
 //
 
-//import UIKit
-//import HealthKit
-//
-//class ViewController: UIViewController {
-//
-//    @IBOutlet weak var tableView: UIStackView!
-//    @IBOutlet weak var heartRateLabel: UILabel!
-//
-//    // 심박수, 수면시간 권한 요청
-//    let healthStore = HKHealthStore()
-//    var query: HKStatisticsCollectionQuery?
-//    var dateValues: [HKStatistics] = []
-//
-//    let typeToRead = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!,
-//                    HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!])
-//    let typeToShare = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!,
-//                    HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!])
-//
-//    var sleepData:[HKCategorySample] = [] // 수면 데이터 저장 배열
-//
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//        self.tableView.dataSource = self
-//
-//    }
-//
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//
-//        healthStore.requestAuthorization(toShare: typeToShare, read: typeToRead) { (success, error) in
-//            if error != nil {
-//                print(error.debugDescription)
-//            } else {
-//                if success {
-//                    print("권한이 허락되었습니다.")
-//                } else {
-//                    print("권한이 아직 없습니다.")
-//                }
-//            }
-//        }
-//    }
-//
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//        // 데이터 필요X -> 쿼리 중지
-//        self.healthStore.stop(query!)
-//    }
-//
-//    func calcDailyHeartRateCountForPastWeek() {
-//        let monday = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
-//        let daily = DateComponents(day: 1)
-//        let exactlySevenDaysAgo = Calendar.current.date(byAdding: DateComponents(day: -7), to: Date())!
-//        let oneWeekAgo = HKQuery.predicateForSamples(withStart: exactlySevenDaysAgo, end: nil, options: .strictStartDate)
-//
-//        self.query = HKStatisticsCollectionQuery(quantityType: stepType,
-//                                                         quantitySamplePredicate: oneWeekAgo,
-//                                                         options: .cumulativeSum,
-//                                                         anchorDate: monday,
-//                                                         intervalComponents: daily)
-//
-//        self.query?.initialResultsHandler = { _, statisticsCollection, _ in
-//            if let statisticsCollection = statisticsCollection {
-//                self.updateUIFromStatistics(statisticsCollection)
-//            }
-//        }
-//
-//        self.query?.statisticsUpdateHandler = { _, _, statisticsCollection, _ in
-//            if let statisticsCollection = statisticsCollection {
-//                self.updateUIFromStatistics(statisticsCollection)
-//            }
-//        }
-//        self.healthStore.execute(query!)
-//    }
-//
-//    func updateUIFromStatistics(_ statisticsCollection: HKStatisticsCollection) {
-//        DispatchQueue.main.async {
-//            self.dateValues = []
-//
-//            let startDate = Calendar.current.date(byAdding: .day, value: -6, to: Date())!
-//            let endDate = Date()
-//
-//            statisticsCollection.enumerateStatistics(from: startDate, to: endDate) { [weak self] (statistics, _) in
-//                    self?.dateValues.append(statistics)
-//                }
-//            self.tableView.reloadInputViews()
-//        }
-//    }
-//}
-//
-//extension ViewController: UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        self.dateValues.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//            guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as? HomeTableViewCell else { return UITableViewCell() }
-//            cell.configure(with: dateValues[indexPath.row])
-//            return cell
-//    }
-//}
+import UIKit
+import HealthKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var heartRateLabel: UILabel!
+    @IBOutlet weak var driveBtn: UIButton!
+    
+    // 심박수, 수면시간 권한 요청
+    let healthStore = HKHealthStore()
+    var heartRateBPM = String()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        authorizeHealthKit()
+        setLabel()
+    }
+    
+    func setLabel() {
+        self.heartRateLabel.text = "HeartRate : \(self.heartRateBPM) BPM"
+    }
+    
+    // healthKit 권한 요청
+    func authorizeHealthKit() {
+        // 심박수, 수면시간 권한 요청
+        let typeToRead = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!,
+                        HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!])
+        let typeToShare = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!,
+                        HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!])
+
+        healthStore.requestAuthorization(toShare: typeToShare, read: typeToRead) { (success, error) in
+            if error != nil {
+                print(error.debugDescription)
+            } else {
+                if success {
+                    print("권한이 허락되었습니다.")
+                    self.getHeartRateData()
+                } else {
+                    print("권한이 아직 없습니다.")
+                }
+            }
+        }
+    }
+    
+    // 심장 박동수 가져오기
+    // https://ios-dev-tech.tistory.com/12
+    // https://www.youtube.com/watch?v=uzJXV_9IBoc
+    func getHeartRateData() {
+        // HKObjectType: HealthKitStore 대한 특정 유형의 데이터 식별 클래스
+        // HKObjectType.quantityType: identifier에 대한 수량 유형 반환
+        guard let sampleType = HKObjectType.quantityType(forIdentifier: .heartRate) else {
+            return
+        }
+        // startDate: 1시간 전 ~ 현재 날짜 시간 정보
+        let startDate = Calendar.current.date(byAdding: .month, value: -1, to: Date())
+        // HKQuery: HealthKit 안 모든 query class 위한 추상 클래스
+        // HKQuery.predicateForSamples: 특정 시간 동안의 특정 데이터 반환
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: Date(), options: .strictEndDate)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+        let query = HKSampleQuery(sampleType: sampleType, predicate: predicate, limit: Int(HKObjectQueryNoLimit), sortDescriptors: [sortDescriptor]) {
+            (sample, result, error) in
+            guard error == nil else {
+                return
+            }
+            let data = result![0] as! HKQuantitySample
+            let unit = HKUnit(from: "count/min")
+            let latesHr = data.quantity.doubleValue(for: unit)
+            self.heartRateBPM = String(format: "%.2f", latesHr)
+            print("HeartRate : \(self.heartRateBPM) BPM")
+            
+            let dateFormator = DateFormatter()
+            dateFormator.dateFormat = "dd/MM/yyyy hh:mm s"
+            let StartDate = dateFormator.string(from: data.startDate)
+            let EndDate = dateFormator.string(from: data.endDate)
+            print("StartDate : \(StartDate) - EndDate : \(EndDate)")
+        }
+        healthStore.execute(query)
+    }
+}
