602d7c8979ce51c147398da7b423180c9e188a63
jiyun B <jiyunbae36@gmail.com>
2023-01-30 03:35:11 +0900

    Feat: add sleep analysis func

---
+++ AntiSleep WatchKit Extension/RowTableController.swift
@@ -0,0 +1,14 @@
+//
+//  RowController.swift
+//  AntiSleep WatchKit Extension
+//
+//  Created by Jiyun Bae on 2023/01/29.
+//
+
+import Foundation
+import WatchKit
+
+class RowTableController: NSObject {
+    
+    @IBOutlet weak var rowLabel: WKInterfaceLabel!
+}
